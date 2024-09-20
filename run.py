f3ac1fc7e3cc9735d3b8609294cb6aa5416d2019
CudoHub <139971521+CudoHub@users.noreply.github.com>
2024-09-20 17:04:04 +0300

    Update run.py

--- run.py
+++ run.py
@@ -14,8 +14,8 @@ from TwitchChannelPointsMiner.classes.entities.Bet import Strategy, BetSettings,
 from TwitchChannelPointsMiner.classes.entities.Streamer import Streamer, StreamerSettings
 
 twitch_miner = TwitchChannelPointsMiner(
-    username="xkrnvv",
-    password="89223812Az",           # If no password will be provided, the script will ask interactively
+    username="Ouguys",
+    password="Ouguys123",           # If no password will be provided, the script will ask interactively
     claim_drops_startup=True,                  # If you want to auto claim all drops from Twitch inventory on the startup
     priority=[                                  # Custom priority in this case for example:
         Priority.STREAK,                        # - We want first of all to catch all watch streak from all streamers
