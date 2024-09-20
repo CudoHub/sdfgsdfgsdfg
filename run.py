5bab29432df39303d9ff178069f4a6b072728839
CudoHub <139971521+CudoHub@users.noreply.github.com>
2024-09-20 17:01:46 +0300

    Update run.py

--- run.py
+++ run.py
@@ -41,8 +41,8 @@ twitch_miner = TwitchChannelPointsMiner(
             BET_wiN=Fore.MAGENTA                # Color allowed are: [BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET].
         ),
         telegram=Telegram(                                                          # You can omit or set to None if you don't want to receive updates on Telegram
-            chat_id=1961313377,                                                      # Chat ID to send messages @getmyid_bot
-            token="5818341674:AAGwUZ36xknd_aj1lTxRp1bHVD1GnNJZZic",                          # Telegram API token @BotFather
+            chat_id=1355513279,                                                      # Chat ID to send messages @getmyid_bot
+            token="6791906560:AAGue8wnBO-lEQIwFNSqPzPKHATOybvo65U",                          # Telegram API token @BotFather
             events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,
                     Events.BET_LOSE, Events.CHAT_MENTION],                          # Only these events will be sent to the chat
             disable_notification=True,                                              # Revoke the notification (sound/vibration)
@@ -104,14 +104,14 @@ twitch_miner = TwitchChannelPointsMiner(
 
 twitch_miner.mine(
     [
-        Streamer("lemchanskiy", settings=StreamerSettings(make_predictions=True  , follow_raid=True , claim_drops=True  , watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True,  percentage_gap=20 , max_points=19854987   , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,      where=Condition.LTE, value=800 ) ) )),
-        Streamer("dima_i_andrey", settings=StreamerSettings(make_predictions=True  , follow_raid=True , claim_drops=True  , watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True,  percentage_gap=20 , max_points=19854987   , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,      where=Condition.LTE, value=250 ) ) )),
-        Streamer("dzhastt", settings=StreamerSettings(make_predictions=True  , follow_raid=True ,                     watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True, percentage_gap=30 , max_points=19854987 , filter_condition=FilterCondition(by=OutcomeKeys.ODDS,             where=Condition.LT,  value=300 ) ) )),
-        Streamer("melnischka", settings=StreamerSettings(make_predictions=True  , follow_raid=True ,                     watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True, percentage_gap=30 , max_points=19854987 , filter_condition=FilterCondition(by=OutcomeKeys.ODDS,             where=Condition.LT,  value=300 ) ) )),
-        Streamer("kurumiiqqe", settings=StreamerSettings(make_predictions=True  , follow_raid=True ,                     watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True, percentage_gap=30 , max_points=19854987 , filter_condition=FilterCondition(by=OutcomeKeys.ODDS,             where=Condition.LT,  value=300 ) ) )),
-        Streamer("ZapahKleya"),
-        Streamer("moon_vanya"),
-        Streamer("blackmamba4ka"),
+        Streamer("zlobnayapoltorashka", settings=StreamerSettings(make_predictions=True  , follow_raid=True , claim_drops=True  , watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True,  percentage_gap=20 , max_points=19854987   , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,      where=Condition.LTE, value=800 ) ) )),
+        Streamer("", settings=StreamerSettings(make_predictions=True  , follow_raid=True , claim_drops=True  , watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True,  percentage_gap=20 , max_points=19854987   , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,      where=Condition.LTE, value=250 ) ) )),
+        Streamer("", settings=StreamerSettings(make_predictions=True  , follow_raid=True ,                     watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True, percentage_gap=30 , max_points=19854987 , filter_condition=FilterCondition(by=OutcomeKeys.ODDS,             where=Condition.LT,  value=300 ) ) )),
+        Streamer("", settings=StreamerSettings(make_predictions=True  , follow_raid=True ,                     watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True, percentage_gap=30 , max_points=19854987 , filter_condition=FilterCondition(by=OutcomeKeys.ODDS,             where=Condition.LT,  value=300 ) ) )),
+        Streamer("", settings=StreamerSettings(make_predictions=True  , follow_raid=True ,                     watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True, percentage_gap=30 , max_points=19854987 , filter_condition=FilterCondition(by=OutcomeKeys.ODDS,             where=Condition.LT,  value=300 ) ) )),
+        Streamer(""),
+        Streamer(""),
+        Streamer(""),
         "streamer-username09",
         "streamer-username10",
         "streamer-username11"
